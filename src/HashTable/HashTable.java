c90c6a651a222a59ddf9ad9f31ac9e5145eab5b2
HarshG777 <harshgaikwadofficial@gmail.com>
2024-09-24 15:40:24 +0530

    HashTable

--- src/HashTable/HashTable.java
+++ src/HashTable/HashTable.java
@@ -22,6 +22,7 @@ public class HashTable{
 
             }
         }
+        System.out.println("\n");
     }
 
     private int hash(String key){
@@ -51,5 +52,15 @@ public class HashTable{
             }
             temp.next = newNode;
         }
+    }
+
+    public int get(String key){
+        int index = hash(key);
+        Node temp = dataMap[index];
+        while (temp != null){
+            if(temp.key == key) return temp.value;
+            temp = temp.next;
+        }
+        return 0;
     }
 }
--- src/HashTable/main.java
+++ src/HashTable/main.java
@@ -6,15 +6,17 @@ public class main {
       //  myHashTable.printTable();
 
         myHashTable.set("Apple",30);
-        myHashTable.set("orange",30);
-        myHashTable.set("banana",30);
-        myHashTable.set("fig",30);
+        myHashTable.set("orange",40);
+        myHashTable.set("banana",50);
+        myHashTable.set("fig",70);
         myHashTable.set("grapes",30);
-        myHashTable.set("watermelon",30);
+        myHashTable.set("watermelon",10);
         myHashTable.set("papaya",90);
         myHashTable.set("lemon",40);
 
         myHashTable.printTable();
+
+        System.out.println(myHashTable.get("lemon"));
 
 
     }
